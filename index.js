d04ca7580d19853c304cab9180010ea45a721a9f
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-06-13 20:07:03 -0500

    paquete: Instalando cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { getLinkPreview, getPreviewFromContent } = require('link-preview-js');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(express.static('public'));
 
